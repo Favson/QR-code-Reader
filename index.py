2081819f35570b7ae60d19a8fc405f0ad5e8145b
Favour Adebisi <adebisimomoluwa@gmail.com>
2024-06-23 21:11:13 +0100

    update index.py

--- index.py
+++ index.py
@@ -1,6 +1,13 @@
-print('Hi!, Welcome to Favson Tech Global')
-age = int(input("Enter Your Age: ").strip());
-if age >= 0 or age <=12:
+print('Hi! Welcome to Favson Tech Global')
+age = int(input("Enter Your Age: "))
+
+if 0 < age <= 12:
     print('Child')
-elif age >= 13 or age <=19:
-    print('')
+elif 13 <= age <= 19:
+    print('Teenager')
+elif 20 <= age <= 64:
+    print('Adult')
+elif age >= 65:
+    print('Senior')
+else:
+    print('Invalid age')
